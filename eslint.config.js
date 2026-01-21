4f2247fb4847bc2f53d80a2b4355293de9d944c7
Andres David Duque Cadena <andduque.ing@gmail.com>
2026-01-21 13:26:26 -0500

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+//  @ts-check
+
+import { tanstackConfig } from '@tanstack/eslint-config'
+
+export default [
+  ...tanstackConfig,
+  {
+    rules: {
+      '@typescript-eslint/no-unnecessary-condition': 'off',
+      'no-shadow': 'off',
+      '@typescript-eslint/no-shadow': 'off',
+    },
+    ignores: ['**/routeTree.gen.ts'],
+  },
+]
